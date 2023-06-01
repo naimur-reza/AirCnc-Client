da54d9046ed0854de75413244f5745f5b262b708
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-01 19:58:08 +0600

    done with imgbb api and added useAuth hook

--- src/pages/Dashboard/AddRoom.jsx
+++ src/pages/Dashboard/AddRoom.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import AddRoomForm from "../../components/Forms/AddRoomForms";
+import { uploadImage } from "../../api/utils";
+import useAuth from "../../api/useAuth";
 
 const AddRoom = () => {
-  return <AddRoomForm />;
+  const [loading, setLoading] = useState(false);
+  const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const location = e.target.location.value;
+    const category = e.target.category.value;
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    const price = e.target.price.value;
+    // const from = dates.startDate;
+    // const to = dates.endDate;
+    // const bed = e.target.bed.value;
+    const guest = e.target.guest.value;
+    const image = e.target.image.files[0];
+
+    // upload image operation
+    uploadImage(image)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        setLoading(false);
+      });
+  };
+  const handleImageChange = (image) => {
+    console.log(image.name);
+    setUploadButtonText(image.name);
+  };
+  return (
+    <AddRoomForm
+      handleSubmit={handleSubmit}
+      loading={loading}
+      uploadButtonText={uploadButtonText}
+      handleImageChange={handleImageChange}
+    />
+  );
 };
 
 export default AddRoom;
