b26d90f84d8230fb65c5be4185e9e2002c6fca67
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-05 16:53:06 +0600

    readme updated installed stripe

---
+++ src/components/Forms/CheckoutForm.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const CheckoutForm = () => {
+  return (
+    <form onSubmit={this.handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#424770",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+export default CheckoutForm;
