966b605fc69c52d63168addee740075d23757c2b
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-06 11:18:37 +0600

    image circle fixed

--- src/components/Shared/Navbar/Avatar.jsx
+++ src/components/Shared/Navbar/Avatar.jsx
@@ -1,18 +1,16 @@
-import { useContext } from 'react'
-import avatarImg from '../../../assets/images/placeholder.jpg'
-import { AuthContext } from '../../../providers/AuthProvider'
+import { useContext } from "react";
+import avatarImg from "../../../assets/images/placeholder.jpg";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const Avatar = () => {
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   return (
     <img
-      className='rounded-full'
+      className="h-9 w-9 rounded-full"
       src={user && user.photoURL ? user.photoURL : avatarImg}
-      alt='profile'
-      height='30'
-      width='30'
+      alt="profile"
     />
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
